0eb198907a7581628b965f40e1862dfe4a474258
叶林 <stonebridge@njfu.edu.cn>
2021-12-09 10:46:42 +0800

    mybatis、mybatis plus、template整合多数据源

--- src/main/java/com/stonebridge/config/JdbcTemplateConfig.java
+++ src/main/java/com/stonebridge/config/JdbcTemplateConfig.java
@@ -10,7 +10,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
 
 @Configuration
-public class DataSourceConfig {
+public class JdbcTemplateConfig {
 
     @Bean(name = "jdbcTemplateMydb")
     @DS("mydb")
